649d955cee2a99d938c42c6521e7b741b9bc52c6
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-01-13 22:52:08 +0800

    adjust : tabel manajement costum

--- resources/js/Pages/Cosrent/Costume/App.jsx
+++ resources/js/Pages/Cosrent/Costume/App.jsx
@@ -13,88 +13,183 @@ export default function App({ datas }) {
         >
             <Head title="Costum" />
 
-            <div className="py-12 flex flex-col gap-6 w-full">
-                {datas.length === 0 && (
-                    <div className="px-4 sm:px-6 lg:px-8 w-full">
-                        <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
-                            <div className="p-6 text-gray-900 dark:text-gray-100">
-                                No Data
-                            </div>
-                        </div>
-                    </div>
-                )}
-
-                {datas.map((data) => (
-                    <div key={data.id} className="sm:px-6 lg:px-8 w-full px-4">
-                        <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800 flex items-center">
-                            {/* gambar */}
-                            <div className="p-6 text-gray-900 dark:text-gray-100">
-                                <img
-                                    src={
-                                        data.image ??
-                                        "https://i.pinimg.com/736x/f2/11/ca/f211ca4c597d85a58673cf5e2fe88daf.jpg"
-                                    }
-                                    alt="images"
-                                    width={60}
+            <div className="p-16">
+                <div className="flex justify-between">
+                    <a href="#">
+                        <button
+                            type="button"
+                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 flex gap-2 items-center"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="w-5 h-5"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M12 4.5v15m7.5-7.5h-15"
                                 />
-                            </div>
+                            </svg>
+                            Tambah Costum
+                        </button>
+                    </a>
 
-                            {/* nama dan deskripsi */}
-                            <div className="p-6 text-gray-900 dark:text-gray-100 pt-0">
-                                <h1 className="text-2xl font-bold mt-0">
-                                    {data.name}
-                                </h1>
-                                <p className="text-sm">{data.description}}</p>
-                            </div>
+                    <a href="#">
+                        <button
+                            type="button"
+                            class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 flex gap-3 items-center"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                class="size-6"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+                                />
+                            </svg>
+                            Export Laporan PDF
+                        </button>
+                    </a>
+                </div>
 
-                            {/* aksi */}
-                            <div className="p-6 text-gray-900 dark:text-gray-100 ml-auto flex gap-3">
-                                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                                    Detail
-                                </button>
-                                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                                    Edit
-                                </button>
-                                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                                    Hapus
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                ))}
-
-                <div className="sm:px-6 lg:px-8 w-full px-4">
-                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800 flex items-center">
-                        {/* gambar */}
-                        <div className="p-6 text-gray-900 dark:text-gray-100">
-                            <img
-                                src="https://i.pinimg.com/736x/f2/11/ca/f211ca4c597d85a58673cf5e2fe88daf.jpg"
-                                alt="test"
-                                width={60}
-                            />
-                        </div>
-
-                        {/* nama dan deskripsi */}
-                        <div className="p-6 text-gray-900 dark:text-gray-100 pt-0">
-                            <h1 className="text-2xl font-bold mt-0">
-                                {"test"}
-                            </h1>
-                            <p className="text-sm">{"test"}</p>
-                        </div>
-
-                        {/* aksi */}
-                        <div className="p-6 text-gray-900 dark:text-gray-100 ml-auto flex gap-3">
-                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                                Detail
-                            </button>
-                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                                Edit
-                            </button>
-                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                                Hapus
-                            </button>
-                        </div>
-                    </div>
+                <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" class="px-6 py-3">
+                                    Image
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Name
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Description
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Price
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Category
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Cosrent
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Size
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Brand
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Status
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Stock
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    <span class="sr-only">Edit</span>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                <th
+                                    scope="row"
+                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                                >
+                                    Apple MacBook Pro 17"
+                                </th>
+                                <td class="px-6 py-4">Silver</td>
+                                <td class="px-6 py-4">Silver</td>
+                                <td class="px-6 py-4">Laptop</td>
+                                <td class="px-6 py-4">Laptop</td>
+                                <td class="px-6 py-4">Laptop</td>
+                                <td class="px-6 py-4">Laptop</td>
+                                <td class="px-6 py-4">Laptop</td>
+                                <td class="px-6 py-4">Laptop</td>
+                                <td class="px-6 py-4">$2999</td>
+                                <td class="px-6 py-4 text-right">
+                                    <div className="flex gap-3 items-center">
+                                        <button
+                                            type="button"
+                                            class="px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 flex gap-2 items-center"
+                                        >
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                fill="none"
+                                                viewBox="0 0 24 24"
+                                                stroke-width="1.5"
+                                                stroke="currentColor"
+                                                class="size-6"
+                                            >
+                                                <path
+                                                    stroke-linecap="round"
+                                                    stroke-linejoin="round"
+                                                    d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                                                />
+                                                <path
+                                                    stroke-linecap="round"
+                                                    stroke-linejoin="round"
+                                                    d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                                                />
+                                            </svg>
+                                            View
+                                        </button>
+                                        <button
+                                            type="button"
+                                            class="px-3 py-2 text-sm font-medium text-center text-white bg-yellow-700 rounded-lg hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-yellow-300 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800 flex gap-2 items-center"
+                                        >
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                fill="none"
+                                                viewBox="0 0 24 24"
+                                                strokeWidth={1.5}
+                                                stroke="currentColor"
+                                                className="size-6"
+                                            >
+                                                <path
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                                                />
+                                            </svg>
+                                            Edit
+                                        </button>
+                                        <button
+                                            type="button"
+                                            class="px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800 flex gap-2 items-center"
+                                        >
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                fill="none"
+                                                viewBox="0 0 24 24"
+                                                stroke-width="1.5"
+                                                stroke="currentColor"
+                                                class="size-6"
+                                            >
+                                                <path
+                                                    stroke-linecap="round"
+                                                    stroke-linejoin="round"
+                                                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                                                />
+                                            </svg>
+                                            Delete
+                                        </button>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </AuthenticatedLayout>
