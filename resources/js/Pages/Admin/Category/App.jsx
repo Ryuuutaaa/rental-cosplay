b0e62235e7a26df1bc2020a0cd7627db94422aa2
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-01-18 10:23:27 +0800

    feat: improve button layout and enhance SVG icons in category management

--- resources/js/Pages/Admin/Category/App.jsx
+++ resources/js/Pages/Admin/Category/App.jsx
@@ -25,7 +25,8 @@ export default function Category({ datas = [] }) {
                 <NavLink href={route("admin.category.create")}>
                     <button
                         type="button"
-                        className="flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                        className="flex gap-2 items-center
+                         text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                     >
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
@@ -116,7 +117,7 @@ export default function Category({ datas = [] }) {
                                     className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
                                 >
                                     <td className="px-6 py-4">{data.name}</td>
-                                    <td className="px-6 py-4 flex items-center gap-2">
+                                    <td className="px-6 py-4 flex gap-2">
                                         {/* Tombol Edit */}
                                         <Link
                                             type="button"
@@ -129,17 +130,11 @@ export default function Category({ datas = [] }) {
                                         >
                                             <svg
                                                 xmlns="http://www.w3.org/2000/svg"
-                                                fill="none"
                                                 viewBox="0 0 24 24"
-                                                strokeWidth={1.5}
-                                                stroke="currentColor"
-                                                className="w-5 h-5"
+                                                fill="currentColor"
+                                                class="size-4"
                                             >
-                                                <path
-                                                    strokeLinecap="round"
-                                                    strokeLinejoin="round"
-                                                    d="M16.862 3.487c.39-.39.902-.586 1.414-.586h.086c.51 0 1.025.195 1.414.586.78.779.78 2.047 0 2.828L7.75 18.414 3 19.75l1.336-4.75L16.862 3.487z"
-                                                />
+                                                <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" />
                                             </svg>
                                             edit
                                         </Link>
@@ -173,7 +168,20 @@ export default function Category({ datas = [] }) {
                                                         e.preventDefault();
                                                     }
                                                 }}
+                                                className="flex gap-2"
                                             >
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 24 24"
+                                                    fill="currentColor"
+                                                    class="size-4"
+                                                >
+                                                    <path
+                                                        fill-rule="evenodd"
+                                                        d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z"
+                                                        clip-rule="evenodd"
+                                                    />
+                                                </svg>
                                                 delete
                                             </button>
                                         </form>
