7de496554658e17962add80433b1c1e13feec618
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-01-18 10:41:54 +0800

    feat: enhance search input with icon and improved padding for better usability

--- resources/js/Pages/Cosrent/Costume/App.jsx
+++ resources/js/Pages/Cosrent/Costume/App.jsx
@@ -69,10 +69,23 @@ export default function App({ datas }) {
                         </button>
                     </a>
 
-                    <div>
+                    <div className="relative">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="currentColor"
+                            className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
+                                clipRule="evenodd"
+                            />
+                        </svg>
+
                         <input
                             type="text"
-                            className="border border-gray-300 rounded-lg px-4 py-2 w-full"
+                            className="border border-gray-300 rounded-lg pl-10 py-2 w-full"
                             placeholder="Search Costume..."
                             value={search}
                             onChange={(e) => setSearch(e.target.value)}
